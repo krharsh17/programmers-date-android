0052f58af2dcb5df2ebabee29d348eb7be887e1c
Kumar Harsh <kharsh39@gmail.com>
2019-12-30 23:12:17 +0530

    Splash UI complete

---
+++ app/src/main/java/in/krharsh17/programmersdate/home/LevelsAdapter.java
@@ -0,0 +1,49 @@
+package in.krharsh17.programmersdate.home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
+import in.krharsh17.programmersdate.R;
+import in.krharsh17.programmersdate.models.Level;
+
+public class LevelsAdapter extends RecyclerView.Adapter<LevelsAdapter.ViewHolder> {
+
+    Context context;
+    private ArrayList<Level> levels;
+
+    public LevelsAdapter(Context context){
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.level_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return levels.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
