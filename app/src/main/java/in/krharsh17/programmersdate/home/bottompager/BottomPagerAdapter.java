3c08eaf136223e5c25d64a7894aac8f1bc3495d0
parthsharma07 <parthcricketer07@gmail.com>
2020-01-07 21:39:23 +0530

    Pose logo activity (#18)
    * logo and pose activity in progress
    
    * logo and pose activity in progress
    
    * logo activity complete
    
    * logo activity complete
    
    * Update CoupleManager.java
    
    * logo activity complete
    
    Co-authored-by: Kumar Harsh <kharsh39@gmail.com>

--- app/src/main/java/in/krharsh17/programmersdate/home/bottompager/BottomPagerAdapter.java
+++ app/src/main/java/in/krharsh17/programmersdate/home/bottompager/BottomPagerAdapter.java
@@ -15,6 +15,7 @@ public class BottomPagerAdapter extends FragmentPagerAdapter {
     public BottomPagerAdapter(FragmentManager fm) {
         super(fm);
         fragments = new ArrayList<>();
+        fragments.add(new DetailFragment().setTaskType(Constants.taskTypeLogo, 1));
     }
 
     public ArrayList<DetailFragment> getFragments() {
